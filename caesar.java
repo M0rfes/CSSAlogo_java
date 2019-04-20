d6a2fff038fb33b2f8a8756eee5c9e6eee53b091
M0rfes <42341113+M0rfes@users.noreply.github.com>
2019-04-20 21:07:10 +0530

    first commit

---
+++ caesar.java
@@ -0,0 +1,46 @@
+//A Java Program to illustrate Caesar Cipher Technique
+class CaesarCipher {
+    // Encrypts text using a shift od s
+    public static StringBuffer encrypt(String text, int s) {
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                char ch = (char) (((int) text.charAt(i) + s - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) text.charAt(i) + s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+
+    // Decript text using a shift od s
+    public static StringBuffer decrypt(String text, int s) {
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                char ch = (char) (((int) text.charAt(i) - s - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) text.charAt(i) - s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        String text = "ATTACKATONCE";
+        int s = 4;
+        System.out.println("Text : " + text);
+        System.out.println("Shift : " + s);
+        StringBuffer cipherText = encrypt(text, s);
+        System.out.println("Cipher: " + cipherText);
+        StringBuffer decrypted = decrypt(cipherText.toString(), s);
+        System.out.println(decrypted);
+    }
+}
